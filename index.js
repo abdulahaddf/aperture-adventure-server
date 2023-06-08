4ca95b60227f79fc6f225a4d6ba30280ce1c5317
abdulahaddf <abdulahad.df1@gmail.com>
2023-06-09 03:31:06 +0600

    add and get class data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -8,6 +9,24 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jp8yltl.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,13 +46,27 @@ async function run() {
     await client.connect();
 
     const usersCollection = client.db("adventureDB").collection("users");
+    const classCollection = client.db("adventureDB").collection("classes");
+
+
+
+
+
+    
+
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+  
+        res.send({ token })
+      })
 
 
 
 
 
     //user related api
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyJWT, async (req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result);
       });
@@ -53,7 +86,17 @@ async function run() {
 
 
 
+// class related apis
+app.get('/class', async (req, res) => {
+  const result = await classCollection.find().toArray();
+  res.send(result);
+})
 
+app.post('/class', async (req, res) => {
+  const newClass = req.body;
+  const result = await classCollection.insertOne(newClass)
+  res.send(result);
+})
 
 
 
